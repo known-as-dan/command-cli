eee41dbdda001910f9728e2723d7caef89d2bf8d
known-as-dan <53865994+known-as-dan@users.noreply.github.com>
2019-10-02 16:12:36 +0000

    revamped comment strategy and added init option for user control

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,3 @@
-/**
- * Command-line command interface.
- */
 interface CommandInterface {
 	name: string;
 	identifier: string;
@@ -8,9 +5,7 @@ interface CommandInterface {
 	commandFunction(args: Array<string>): void;
 }
 
-/**
- * Command-line command class.
- */
+let commands: Array<Command> = [];
 export class Command implements CommandInterface {
 	name: string;
 	identifier: string;
@@ -18,53 +13,38 @@ export class Command implements CommandInterface {
 	commandFunction: (args: Array<string>) => void;
 	
 	/**
-	 * Command-line command class constructor.
 	 * @param name Representative command name, so keep this a bit more descriptive.
 	 * @param identifier Command identifier that's to be used when firing the command.
 	 * @param usage Description of the command parameters. | Example: "<text>" or "<file_path>" etc...
-	 * - Pass "null" or an empty string if you don't need this.
+	 * - Pass an empty string if you don't need this.
 	 * @param commandFunction function to be executed when the command is used.
 	 */
 	constructor(name: string, identifier: string, usage: string, commandFunction: (args: Array<string>) => void) {
 		this.name = name;
 		this.identifier = identifier;
-		this.usage = usage || ""; // Defaulting to an empty string if no command parameter usage description was provided
+		this.usage = usage || "";
 		this.commandFunction  = commandFunction;
 
-		// Pushing the new command object into the 'commands' array.
 		commands.push(this);
 	}
 
-	/**
-	 * Returns the command's name.
-	 */
 	getName(): string {
 		return this.name;
 	}
 
-	/**
-	 * Returns the command's name.
-	 */
 	getIdentifier(): string {
 		return this.identifier;
 	}
 
-	/**
-	 * Returns the command's usage example.
-	 */
 	getUsage(): string {
 		return this.usage;
 	}
 
-	/**
-	 * Returns the command's function.
-	 */
 	getFunction(): (args: Array<string>) => void {
 		return this.commandFunction;
 	}
 
 	/**
-	 * Executes the command's function with the passed arguments.
 	 * @param args Command arguments.
 	 */
 	execute(args: Array<string>): void {
@@ -73,26 +53,25 @@ export class Command implements CommandInterface {
 	}
 }
 
-/**
- * Holds all command objects.
- */
-let commands: Array<Command> = [];
-
-/**
- * Returns an array with all existing "Command" objects.
- */
-function getCommands(): Array<Command> {
+function getAllCommands(): Array<Command> {
 	return commands;
 }
 
 /**
  * Find a command object based on its identifier and/or name.
  * - Returns "null" if no match was found.
- * @param identifier Command identifier.
- * @param name Command name(Optional).
+ * @param identifier Command identifier. (Optional)
+ * @param name Command name. (Optional)
  */
-function getCommand(identifier: string, name?: string): Command | null {
-	const commands = getCommands();
+function getCommand(identifier?: string, name?: string): Command | null {
+	if (!(identifier && name)) {
+		return null;
+	}
+
+	identifier = identifier || "";
+	name = name || "";
+
+	const commands = getAllCommands();
 	let command: Command;
 	let command_identifier: string;
 	let command_name: string;
@@ -100,22 +79,54 @@ function getCommand(identifier: string, name?: string): Command | null {
 		command = commands[i];
 		command_identifier = command.getIdentifier();
 		command_name = command.getName();
-		// Checking if either the identifier or name matches the current command.
 		if ((identifier === command_identifier) || (name === command_name)) {
 			return command;
 		}
 	}
-	// We can assume that if we're here(beyond the for loop), that we haven't
-	// found a match for the specified arguments, so we just return "null" to
-	// indicate a failiure.
+
 	return null;
+}
+
+/**
+ * 
+ * @param help Set whether or not you want the program to automatically
+ * print out the list of available commands, if no command was used.
+ * This is "true" by default. (Optional)
+ * @param args You can choose to pass your own set of arguments,
+ * but by default, the program will use "process.argv". (Optional)
+ */
+export function init(print_help?: boolean, args?: Array<string>): void {
+	print_help = print_help || true; // "print_help" defaults to "true".
+	
+	if (args) {
+		if (args.length > 0) {
+			const called_command = args[0]
+			args.splice(0, 1)
+			fire(called_command, args);
+		} else {
+			if (print_help) {
+				help();
+			}
+		}
+	} else if (process.argv) {
+		args = process.argv;
+		if (args.length > 2) { // ignoring the initial two arguments("node" & "path/to/file.js")
+			const called_command = args[2];
+			args.splice(0, 3);
+			fire(called_command, args);
+		} else {
+			if (print_help) {
+				help();
+			}
+		}
+	}
 }
 
 /**
  * Print a list of all commands and their
  * information in the console.
  */
-function help(): void {
+export function help(): void {
 	// format of the command info print-out
 	let format = "[command_name] command_identifier command_usage"
 
@@ -124,55 +135,39 @@ function help(): void {
 
 	// Grabbing the array of commands, iterating over them
 	// and printing out their information.
-	const commands: Array<Command> = getCommands();
-	let command_name: string;
-	let command_identifier: string;
-	let command_usage: string;
-	let info: string; // Command info to print.
-	commands.forEach((command) => {
-		command_name = command.getName();
-		command_identifier = command.getIdentifier();
-		command_usage = command.getUsage();
-		info = `[${command_name}] ${command_identifier} ${command_usage}`
-		console.log(info);
-	});
+	const commands: Array<Command> = getAllCommands();
+	if (commands.length > 0) {
+		let command_name: string;
+		let command_identifier: string;
+		let command_usage: string;
+		let info: string; // Command info to print.
+		commands.forEach((command) => {
+			command_name = command.getName();
+			command_identifier = command.getIdentifier();
+			command_usage = command.getUsage();
+			info = `[${command_name}] ${command_identifier} ${command_usage}`
+			console.log(info);
+		});
+	} else {
+		console.log(" - There are no commands!");
+	}
 
 	console.log("\n")
 }
 
 /**
- * This function is used to execute all kinds of commmands.
+ * Executes the specified commands.
  * @param command_identifier Command identifier.
- * @param args Command arguments(Optional, defaults to an empty array: "[]").
+ * @param args Command arguments. (Optional)
  */
 export function fire(command_identifier: string, args?: Array<string>): void {
-	// If no command was used, then we print out the
-	// help list with all the commmands.
-	if (command_identifier === "") {
-		help();
-	} else {
-		args = args || []; // Setting "args" to default to an empty array if it wasn't passed.
+	args = args || [];
 
-		// Finding the specified command by its identifier and executing it with the passed arguments.
-		// If the named command wasn't found, an error message is printed out into the console.
-		const command: Command | null = getCommand(command_identifier);
-		if (command instanceof Command) {
-			command.execute(args);
-		} else {
-			const message: string = `Could not find a command named '${command_identifier}'!`;
-			console.log(message);
-		}
+	const command: Command | null = getCommand(command_identifier);
+	if (command instanceof Command) {
+		command.execute(args);
+	} else {
+		const message: string = `Could not find a command named '${command_identifier}'! Use the command "help" to get a list of all available commands.`;
+		console.log(message);
 	}
 }
-
-
-//
-//			Default Commands
-//
-
-/**
- * Lists all available commands and their usage examples.
- */
-new Command("Help", "help", "", (args: Array<string>) => {
-	help();
-});
--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,19 @@
-import { fire, Command } from "./command";
+import { Command, fire, help, init } from "./command";
 
-export { fire, Command };
+export { Command, fire, help, init };
 
 /**
- * This is the main function, it's executed once the rest
- * of the code is finished processing. The main function
- * holds most runtime related logic/behaviour.
- * @param args Arguments(generally command-line arguments).
+ * @param args Arguments.
  */
 function main(args: Array<string>): number {
-	// Checking if at least a single argument was passed.
-	// First two are generally just node and the javascript
-	// file, so we're looking for more than that(2).
-	if (args.length > 2) {
-		const called_command: string = args[2];
-		args.splice(0, 3); // Slicing out the initial two arguments(node and js file) and the command, leaving only the arguments.
-		fire(called_command, args); // Firing the command and passing the left-over arguments to it.
-	} else {
-		fire("help"); // Firing the "help" command to print out all available commands if no command was used.
-	}
+	init();
 
 	return 0; // By default we return 0(reference=exit_code) to indicate successful code execution.
 }
 
-// Calling main function + passing command-line arguments.
-const args: Array<string> = process.argv // Command-line arguments.
-const exit_code: number = main(args) // Calling the main function + Storing the exit code.
+// NOTE: This part should ALWAYS be at the end of the file.
+const exit_code: number = main(process.argv)
+// --------------------------------------------------------
 
 // TO-DO:
-//
 // - Implement main function exit code functionality.
