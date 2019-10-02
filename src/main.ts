d01274e03d212bc54ed7604684a285fb4f3b5e82
known-as-dan <53865994+known-as-dan@users.noreply.github.com>
2019-10-02 16:47:49 +0000

    messed with main func

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,10 @@ export { Command, fire, help, init };
  * @param args Arguments.
  */
 function main(args: Array<string>): number {
-	init();
-
-	return 0; // By default we return 0(reference=exit_code) to indicate successful code execution.
+	return 0; // 0 == successful execution
 }
 
-// NOTE: This part should ALWAYS be at the end of the file.
+// NOTE: This part should ALWAYS be at the end of the file!
 const exit_code: number = main(process.argv)
 // --------------------------------------------------------
 
